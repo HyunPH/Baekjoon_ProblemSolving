8acb48d72e724d235d47de7f4d4bf60f5e28bd4d
Lee-TaeHyun <112704330+HyunPH@users.noreply.github.com>
2023-03-05 13:17:32 +0900

    Update 숫자 카드.c

--- 백준/Silver/10815. 숫자 카드/숫자 카드.c
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.c
@@ -1,53 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-int compare(const void *num1,const void *num2)
+
+int compare(const void* a, const void* b)
 {
-    if (*(int*)num1 < *(int*)num2)
-        return -1;   
- 
-    if (*(int*)num1 > *(int*)num2)
-        return 1;
-    else
-    return 0;
+	if (*(int *)a > *(int*)b)
+		return 1;
+	else if (*(int*)a < *(int*)b)
+		return -1;
+	
+	return 0;
 }
-int binary_search(int *arr,int N,int num)
+
+
+int bin_search(int *arr, int key, int len)
 {
-    int start = 0;
-    int end = N;
-    int mid = (start + end) / 2;
-    while (start<=end)
-    {
-        if (arr[mid] == num)
-            return 1;
-        else if (arr[mid] < num)
-            start = mid + 1;
-        else
-            end = mid - 1;
-        mid = (start + end) / 2;
-    }
-    return 0;
+	int begin = 0, end = len - 1, mid ;
+
+	while (begin <= end)
+	{
+		mid = (begin + end) / 2;
+		if (arr[mid] == key)
+			return 1;
+		
+		if (key < arr[mid])
+			end = mid - 1;
+		else if (key > arr[mid])
+			begin = mid + 1;	
+	}
+	// 없을 경우
+	return 0;
 }
+
 int main()
 {
-    int *arr, *arr2;
-    int i;
-    int N, M;
-    scanf("%d", &N);
-    arr = (int*)malloc(sizeof(int)*N);
-    for (i = 0;i < N;i++)
-        scanf("%d", &arr[i]);
-    scanf("%d", &M);
-    arr2 = (int*)malloc(sizeof(int)*M);
-    for (i = 0;i < M;i++)
-        scanf("%d", &arr2[i]);
-    qsort(arr, N, sizeof(int),compare);
- 
-    for (i = 0;i < M;i++)
-    {
-        if (binary_search(arr, N, arr2[i]))
-            printf("1 ");
-        else
-            printf("0 ");
-    }
-    
+	int N, M;
+	//int people_num[501], integer_num[501];
+	int *people_num, *integer_num;
+
+	// 상근 카드 입력
+	scanf(" %d", &N);
+	people_num = (int*)calloc(N, sizeof(int));
+	for (int i = 0; i < N; i++)
+		scanf(" %d", &people_num[i]);
+	
+	// 정수 입력
+	scanf(" %d", &M);
+	integer_num = (int*)calloc(M, sizeof(int));
+	for (int i = 0; i < M; i++)
+		scanf(" %d", &integer_num[i]);
+
+
+	// 오름차순 정렬
+	qsort(people_num, N, sizeof(int), compare);
+
+
+	// 결과 출력
+	for (int i = 0; i < M; i++)
+		printf("%d ", bin_search(people_num, integer_num[i], N));
+
+	free(people_num);
+	free(integer_num);
+	return 0;
 }
