397add1defea941345f9671090c1e43108d3e74c
Lee-TaeHyun <112704330+HyunPH@users.noreply.github.com>
2023-03-12 11:04:03 +0900

    Create 체스판 다시 칠하기.c

---
+++ 백준/Silver/1018체스판 다시 칠하기/체스판 다시 칠하기.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main()
+{
+	int N, M, count = 0, min_count;
+	char start;
+	char board[50][51], count_arr[2][43][43];
+
+	scanf(" %d %d", &N, &M);
+	for (int i = 0; i < N; i++)
+		scanf("%s", &board[i]);
+
+
+	for (int eight_i = 8; eight_i <= N; eight_i++)
+	{
+		for (int eight_j = 8; eight_j <= M; eight_j++)
+		{
+			// 시작 짝,홀 일때 두번
+			for (int k = 0; k < 2; k++) 
+			{
+				// 짝수, 홀수 시작 결정.
+				if (k == 0) start = 'W';
+				else if (k == 1) start = 'B';
+				
+				// 8*8 배열 검사.
+				count = 0;
+				for (int i = eight_i - 8; i < eight_i; i++)
+				{
+					for (int j = eight_j - 8; j < eight_j; j++)
+					{
+						if ((i + j) % 2 == 1 && board[i][j] == start)
+							count++;
+						else if ((i + j) % 2 == 0 && board[i][j] != start)
+							count++;
+					}
+				}
+				//printf("k:%d eight_i:%d eight_j:%d count:%d\n", k, eight_i-8, eight_j-8, count);
+				count_arr[k][eight_i - 8][eight_j - 8] = count;
+			}
+		}
+	}
+
+	// 최소값 찾기
+	min_count = count_arr[0][0][0];
+	for (int k = 0; k < 2; k++)
+	{
+		for (int i = 0; i <= N - 8; i++)
+		{
+			for (int j = 0; j <= M - 8; j++)
+			{
+				//printf("k:%d i:%d j:%d count:%d\n", k, i, j, count_arr[k][i][j]);
+				if (count_arr[k][i][j] < min_count)
+				{
+					min_count = count_arr[k][i][j];
+				}
+			}
+		}
+	}
+	
+	printf("%d\n", min_count);
+	return 0;
+}
